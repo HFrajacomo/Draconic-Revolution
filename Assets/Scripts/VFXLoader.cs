b26523336af02a65d955909cdd5f862b63c3dc7f
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2020-08-29 04:44:52 -0300

    Added VFXLoader class
    - This class is used to maintain a reference to all VFX Instances in the code, that might need to be updated/destroyed
    - Added a TESTONLY script in testing scene to do C# testing without the need to call the ChunkLoader

---
+++ Assets/Scripts/VFXLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXLoader : MonoBehaviour
+{
+	public Dictionary<ChunkPos, Dictionary<string, GameObject>> data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		data = new Dictionary<ChunkPos, Dictionary<string, GameObject>>(); 
+    }
+
+    // Registers a new chunk to VFXLoader
+    public void NewChunk(ChunkPos pos){
+    	data.Add(pos, new Dictionary<string, GameObject>());
+    }
+
+    // Unregisters a chunk from VFXLoader
+    public void RemoveChunk(ChunkPos pos){
+    	data.Remove(pos);
+    }
+
+    // Adds GameObject to Chunk Dict and sets it to active
+    public void Add(ChunkPos pos, GameObject go, bool active=true){
+    	data[pos].Add(go.name, go);
+
+    	if(!active){
+    		data[pos][go.name].SetActive(false);
+    	}
+    }
+
+    // Removes and GameObject from Chunk Dict
+    public void Remove(ChunkPos pos, string name){
+    	data[pos].Remove(name);
+    }
+
+}
