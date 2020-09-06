442c8882ed41158cfb199f6263cac07923796328
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2020-09-06 01:17:41 -0300

    Added Visual LiquidMesh to Chunk Building

--- Assets/Scripts/LiquidMeshData.cs
+++ Assets/Scripts/LiquidMeshData.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public static class LiquidMeshData
 {
-	public static Vector3[,] verticesOnState = {
+
+	/*
+	all vertices of all possible states in a liquid mesh
+	*/
+	public static Vector3[][] verticesOnState = {
 
 		// State 0 (Still 3)
-		{new Vector3(1, 1, 1),
+		new Vector3[] {new Vector3(1, 1, 1),
     	new Vector3(-1, 1, 1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -17,7 +21,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
 		// State 1 (Still 2)
-		{new Vector3(1, 1, 1),
+		new Vector3[] {new Vector3(1, 1, 1),
     	new Vector3(0, 1, 1),
     	new Vector3(0, 0, 1),
     	new Vector3(1, 0, 1),
@@ -27,7 +31,7 @@ public static class LiquidMeshData
     	new Vector3(0, 0, 0)},
 
 		// State 2 (Still 1)
-		{new Vector3(0.5f, 0.5f, 0.5f),
+		new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
     	new Vector3(0, 0.5f, 0.5f),
     	new Vector3(0, 0, 0.5f),
     	new Vector3(0.5f, 0, 0.5f),
@@ -37,7 +41,7 @@ public static class LiquidMeshData
     	new Vector3(0, 0, 0)},
 
     	// State 3 (N2)
-    	{new Vector3(1,0,1),
+    	new Vector3[] {new Vector3(1,0,1),
     	new Vector3(-1,0,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -47,7 +51,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 4 (NE2)
-    	{new Vector3(1,0,1),
+    	new Vector3[] {new Vector3(1,0,1),
     	new Vector3(-1,1,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -57,7 +61,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 5 (E2)
-    	{new Vector3(1,0,1),
+    	new Vector3[] {new Vector3(1,0,1),
     	new Vector3(-1,1,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -67,7 +71,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 6 (SE2)
-    	{new Vector3(1,1,1),
+    	new Vector3[] {new Vector3(1,1,1),
     	new Vector3(-1,1,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -77,7 +81,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 7 (S2)
-    	{new Vector3(1,1,1),
+    	new Vector3[] {new Vector3(1,1,1),
     	new Vector3(-1,1,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -87,7 +91,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 8 (SW2)
-    	{new Vector3(1,1,1),
+    	new Vector3[] {new Vector3(1,1,1),
     	new Vector3(-1,1,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -97,7 +101,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 9 (W2)
-    	{new Vector3(1,1,1),
+    	new Vector3[] {new Vector3(1,1,1),
     	new Vector3(-1,0,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -107,7 +111,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 10 (NW2)
-    	{new Vector3(1,1,1),
+    	new Vector3[] {new Vector3(1,1,1),
     	new Vector3(-1,0,1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
@@ -117,7 +121,7 @@ public static class LiquidMeshData
     	new Vector3(-1, -1, -1)},
 
     	// State 11 (N1)
-    	{new Vector3(0.5f,0,0.5f),
+    	new Vector3[] {new Vector3(0.5f,0,0.5f),
     	new Vector3(-0.5f,0,0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
@@ -128,18 +132,18 @@ public static class LiquidMeshData
 
 
     	// State 12 (NE1)
-    	{new Vector3(0.5f,0,0.5f),
+    	new Vector3[] {new Vector3(0.5f,0,0.5f),
     	new Vector3(-0.5f,0.5f,0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
     	new Vector3(-0.5f, 0.5f, -0.5f),
-    	new Vector3(0.5f, 0.5, -0.5f),
+    	new Vector3(0.5f, 0.5f, -0.5f),
     	new Vector3(0.5f, -0.5f, -0.5f),
     	new Vector3(-0.5f, -0.5f, -0.5f)},
 
     	// State 13 (E1)
-    	{new Vector3(0.5f, 0, 0.5f),
-    	new Vector3(-0.5f, 0.5, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0, 0.5f),
+    	new Vector3(-0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
     	new Vector3(-0.5f, 0.5f, -0.5f),
@@ -148,8 +152,8 @@ public static class LiquidMeshData
     	new Vector3(-0.5f, -0.5f, -0.5f)},
 
     	// State 14 (SE1)
-    	{new Vector3(0.5f, 0.5f, 0.5f),
-    	new Vector3(-0.5f, 0.5, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
+    	new Vector3(-0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
     	new Vector3(-0.5f, 0.5f, -0.5f),
@@ -158,8 +162,8 @@ public static class LiquidMeshData
     	new Vector3(-0.5f, -0.5f, -0.5f)},   
 
     	// State 15 (S1)
-    	{new Vector3(0.5f, 0.5f, 0.5f),
-    	new Vector3(-0.5f, 0.5, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
+    	new Vector3(-0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
     	new Vector3(-0.5f, 0, -0.5f),
@@ -168,8 +172,8 @@ public static class LiquidMeshData
     	new Vector3(-0.5f, -0.5f, -0.5f)},
 
     	// State 16 (SW1)
-    	{new Vector3(0.5f, 0.5f, 0.5f),
-    	new Vector3(-0.5f, 0.5, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
+    	new Vector3(-0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
     	new Vector3(-0.5f, 0, -0.5f),
@@ -178,7 +182,7 @@ public static class LiquidMeshData
     	new Vector3(-0.5f, -0.5f, -0.5f)}, 
 
     	// State 17 (W1)
-    	{new Vector3(0.5f, 0.5f, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, 0, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
@@ -188,7 +192,7 @@ public static class LiquidMeshData
     	new Vector3(-0.5f, -0.5f, -0.5f)},
 
     	// State 18 (NW1)
-    	{new Vector3(0.5f, 0.5f, 0.5f),
+    	new Vector3[] {new Vector3(0.5f, 0.5f, 0.5f),
     	new Vector3(-0.5f, 0, 0.5f),
     	new Vector3(-0.5f, -0.5f, 0.5f),
     	new Vector3( 0.5f, -0.5f,  0.5f),
@@ -197,24 +201,52 @@ public static class LiquidMeshData
     	new Vector3(0.5f, -0.5f, -0.5f),
     	new Vector3(-0.5f, -0.5f, -0.5f)},
 	
-		// States 19 & 20 (Still)
-		{new Vector3(1, 1, 1),
+		// States 19 & 20 (Falling 3 and 2)
+		new Vector3[] {new Vector3(1, 1, 1),
     	new Vector3(-1, 1, 1),
     	new Vector3(-1, -1, 1),
     	new Vector3(1, -1, 1),
     	new Vector3(-1, 1, -1),
     	new Vector3(1, 1, -1),
     	new Vector3(1, -1, -1),
-    	new Vector3(-1, -1, -1)},
+    	new Vector3(-1, -1, -1)}
 	};
 
-	public static Vector3[] GetByState(short? s){
+	public static int[][] faceTriangles = {
+	  new int[] {0,1,2,3},
+	  new int[] {5,0,3,6},
+	  new int[] {4,5,6,7},
+	  new int[] {1,4,7,2},
+	  new int[] {5,4,1,0},
+	  new int[] {3,2,7,6}
+	};
+
+	// Gets the vertices of a given state in a liquid
+	public static Vector3[] VertsByState(int dir, ushort? s, Vector3 pos, float scale=0.5f){
+		Vector3[] fv = new Vector3[4];
+
 		if(s == null){
-			return verticesOnState[0];
+		    for (int i = 0; i < fv.Length; i++)
+		    {
+		      fv[i] = (verticesOnState[0][faceTriangles[dir][i]] * scale) + pos;
+		    }
+			return fv;
 		}
 
-		if(s >= 0 && s <=2){
-			return verticesOnState[0];
+
+		if(s == 19 || s == 20){
+		    for (int i = 0; i < fv.Length; i++)
+		    {
+		      fv[i] = (verticesOnState[19][faceTriangles[dir][i]] * scale) + pos;
+		    }
+			return fv;
+		}
+		else{
+		    for (int i = 0; i < fv.Length; i++)
+		    {
+		      fv[i] = (verticesOnState[(int)s][faceTriangles[dir][i]] * scale) + pos;
+		    }
+			return fv;
 		}
 	}
 }
