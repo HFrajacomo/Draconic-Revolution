414139d9de4b2239d5f4f13a745091fa210ffc80
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2021-01-14 13:29:50 -0300

    Added simple Client and Server example
    - Can connect only

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+	public TcpClient socket;
+
+	private const int sendBufferSize = 4096; // 4KB
+	private const int receiveBufferSize = 4096*4096; // 2MB
+
+	private NetworkStream stream;
+	private byte[] receiveBuffer;
+
+	public string ip = "127.0.0.1";
+	public int port = 33000;
+
+	public void Start(){
+		socket = new TcpClient();
+
+		receiveBuffer = new byte[receiveBufferSize];
+		this.Connect();		
+	}
+
+	/*
+	public Client(){
+
+	}
+	*/
+
+	public void Connect(){
+		socket.BeginConnect(this.ip, this.port, ConnectCallback, socket);		
+
+	}
+
+	private void ConnectCallback(IAsyncResult result){
+		socket.EndConnect(result);
+
+		if(!socket.Connected)
+			return;
+
+		stream = socket.GetStream();
+
+		stream.BeginRead(receiveBuffer, 0, receiveBufferSize, ReceiveCallback, null);
+		Debug.Log(receiveBuffer);
+	}
+
+	private void ReceiveCallback(IAsyncResult result){
+		try{
+			int bytesReceived = stream.EndRead(result);
+
+			if(bytesReceived <= 0){
+				return;
+			}
+
+			byte[] data = new byte[bytesReceived];
+			Array.Copy(receiveBuffer, data, bytesReceived);
+		}
+		catch{
+			// Disconnect
+		}
+	}
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Burst;
+
+public class Server : MonoBehaviour
+{
+	public int maxPlayers = 8;
+	public int port = 33000;
+	private TcpListener listener;
+	private bool isLocal = true;
+	public Dictionary<int, TcpClient> connections;
+	private int currentCode = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	connections = new Dictionary<int, TcpClient>();
+
+    	if(!this.isLocal)
+        	this.listener = new TcpListener(IPAddress.Any, this.port);
+        else
+        	this.listener = new TcpListener(IPAddress.Loopback, this.port);
+
+        print("Starting Server");
+        listener.Start();
+        listener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+    }
+
+
+
+    // Callback function for TCPConnect
+    private void TCPConnectCallback(IAsyncResult result){
+    	TcpClient client = this.listener.EndAcceptTcpClient(result);
+    	this.connections[currentCode] = client;
+    	this.currentCode++;
+
+    	Debug.Log(client.ToString() + " has connected");
+    	listener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+    }
+}
