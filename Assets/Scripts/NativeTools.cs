9093cf1b19570126d208dd33f26431c47454ec4a
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2022-02-08 20:39:14 -0300

    Added NativeTools
    - Unsafe C code to handle memory swapping in NativeContainers to Managed memory

---
+++ Assets/Scripts/NativeTools.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using Unity.Collections;
+
+public static class NativeTools
+{
+
+    /*
+    Uses unsafe code to copy all elements in NativeArray to a normal array
+    */
+    public static unsafe T[] CopyToManaged<T>(NativeArray<T> array) where T: struct{
+        int size = array.Length;
+        int byteSize = size * UnsafeUtility.SizeOf<T>();
+
+        T[] outputArray = new T[size];
+    
+        void* inputBuffer = UnsafeUtility.AddressOf(ref outputArray[0]);
+        void* outBuffer = array.GetUnsafePtr();
+
+        UnsafeUtility.MemCpy(inputBuffer, outBuffer, byteSize);
+        return outputArray;
+    }
+    /*
+    public static unsafe List<T> CopyToManaged<T>(NativeList<T> list) where T: struct{
+        int size = list.Length;
+        int byteSize = size * UnsafeUtility.SizeOf<T>();
+
+        List<T> outputList = new List<T>();
+    
+        void* inputBuffer = UnsafeUtility.AddressOf(ref outputList);
+        void* outBuffer = list.GetUnsafePtr();
+
+        UnsafeUtility.MemCpy(inputBuffer, outBuffer, byteSize);
+        return outputList;
+    }
+    */
+
+    /*
+    Uses unsafe code to copy all elements in NativeArray to a normal array
+    */
+    public static unsafe NativeArray<T> CopyToNative<T>(T[] array) where T: struct{
+        int size = array.Length;
+        int byteSize = size * UnsafeUtility.SizeOf<T>();
+
+        NativeArray<T> outputArray = new NativeArray<T>(size, Allocator.TempJob);
+    
+        void* inputBuffer = outputArray.GetUnsafePtr();
+        void* outBuffer = UnsafeUtility.AddressOf(ref array[0]);
+
+        UnsafeUtility.MemCpy(inputBuffer, outBuffer, byteSize);
+        return outputArray;
+    }
+    public static unsafe NativeList<T> CopyToNative<T>(List<T> list) where T: struct{
+        int size = list.Count;
+        int byteSize = size * UnsafeUtility.SizeOf<T>();
+
+        NativeList<T> outputList = new NativeList<T>(0, Allocator.TempJob);
+    
+        void* inputBuffer = outputList.GetUnsafePtr();
+        void* outBuffer = UnsafeUtility.AddressOf(ref outputList);
+
+        UnsafeUtility.MemCpy(inputBuffer, outBuffer, byteSize);
+        return outputList;
+    }
+}
