fbacb15c9a97df59e7aef7c272951ef5bf220b4e
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2022-06-20 03:15:04 -0300

    Added better menu navigation
    - Main Menu's buttons can now be navigated via Arrow Keys
    - SinglePlayer and MultiPlayer menus now are tab navigated

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -16,7 +16,6 @@ public class MainMenu : MonoBehaviour
 
 	// Unity Reference
 	public GameObject skybox;
-
 	public GameObject mainMenu;
 	public GameObject singleplayerMenu;
 	public GameObject multiplayerMenu;
@@ -30,13 +29,31 @@ public class MainMenu : MonoBehaviour
 	public Text multi_renderField;
 	public Text multi_accountField;
 	public TextMeshProUGUI fullbrightText;
+	public InputField single_nameInput;
+	public InputField single_seedInput;
+	public InputField single_renderInput;
+	public InputField multi_ipInput;
+	public InputField multi_accountInput;
+	public InputField multi_renderInput;
+
+	// Initial Button
+	public Button singleplayerButton;
+	public Button singleplayerPlayButton;
+	public Button multiplayerPlayButton;
 
 	// Flags
 	private static bool firstLoad = true;
+	private static MenuCode code = MenuCode.MAIN;
 
 	// Color Constants
 	private Color GREEN = new Color(0.3f, 0.9f, 0.2f);
 	private Color RED = new Color(0.3f, 0.0f, 0.0f);
+
+	// Menu Maps
+	private int currentSelection = 0;
+	private Dictionary<int, Selectable> singlePlayerMap = new Dictionary<int, Selectable>();
+	private Dictionary<int, Selectable> multiPlayerMap = new Dictionary<int, Selectable>();
+	private Dictionary<int, Selectable> optionsMap = new Dictionary<int, Selectable>();
 
 	public void OnApplicationQuit(){
 		BlockEncyclopediaECS.Destroy();
@@ -53,8 +70,35 @@ public class MainMenu : MonoBehaviour
 
 		MainMenu.firstLoad = false;
 
+		CreateSinglePlayerMap();
+		CreateMultiPlayerMap();
 		OpenMainMenu();
 	}
+
+	public void Update(){
+		if(Input.GetKeyDown(KeyCode.Tab)){
+			switch(MainMenu.code){
+				case MenuCode.MAIN:
+					break;
+				case MenuCode.SINGLEPLAYER:
+					if(IncrementTab())
+						singlePlayerMap[this.currentSelection].Select();
+					break;
+				case MenuCode.MULTIPLAYER:
+					if(IncrementTab())
+						multiPlayerMap[this.currentSelection].Select();
+					break;
+				case MenuCode.OPTIONS:
+					if(IncrementTab())
+						optionsMap[this.currentSelection].Select();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+
 
 	public void UnloadMemory(){
 		System.GC.Collect();
@@ -115,19 +159,29 @@ public class MainMenu : MonoBehaviour
 
 	public void OpenMainMenu(){
 		ChangeVisibleMenu(this.mainMenu);
+		singleplayerButton.Select();
+		MainMenu.code = MenuCode.MAIN;
 	}
 
 	public void OpenSingleplayerMenu(){
 		ChangeVisibleMenu(this.singleplayerMenu);
+		this.currentSelection = 0;
+		single_nameInput.Select();
+		MainMenu.code = MenuCode.SINGLEPLAYER;
 	}
 
 	public void OpenMultiplayerMenu(){
 		ChangeVisibleMenu(this.multiplayerMenu);
+		this.currentSelection = 0;
+		multi_ipInput.Select();
+		MainMenu.code = MenuCode.MULTIPLAYER;
 	}
 
 	public void OpenOptionsMenu(){
 		SetFullBrightColor();
 		ChangeVisibleMenu(this.optionsMenu);
+		this.currentSelection = 0;
+		MainMenu.code = MenuCode.OPTIONS;
 	}
 
 	public void ExitGame(){
@@ -153,4 +207,58 @@ public class MainMenu : MonoBehaviour
 		else
 			fullbrightText.color = RED;		
 	}
+
+	private void CreateSinglePlayerMap(){
+		this.singlePlayerMap.Add(0, single_nameInput);
+		this.singlePlayerMap.Add(1, single_seedInput);
+		this.singlePlayerMap.Add(2, single_renderInput);
+		this.singlePlayerMap.Add(3, singleplayerPlayButton);
+	}
+
+	private void CreateMultiPlayerMap(){
+		this.multiPlayerMap.Add(0, multi_ipInput);
+		this.multiPlayerMap.Add(1, multi_accountInput);
+		this.multiPlayerMap.Add(2, multi_renderInput);
+		this.multiPlayerMap.Add(3, multiplayerPlayButton);
+	}
+
+	private bool IncrementTab(){
+		switch(MainMenu.code){
+			case MenuCode.MAIN:
+				return false;
+			case MenuCode.SINGLEPLAYER:
+				if(this.singlePlayerMap.Count == 0)
+					return false;
+
+				this.currentSelection++;
+				if(this.currentSelection >= this.singlePlayerMap.Count)
+					this.currentSelection = 0;
+				return true;
+			case MenuCode.MULTIPLAYER:
+				if(this.multiPlayerMap.Count == 0)
+					return false;
+
+				this.currentSelection++;
+				if(this.currentSelection >= this.multiPlayerMap.Count)
+					this.currentSelection = 0;
+				return true;
+			case MenuCode.OPTIONS:
+				if(this.optionsMap.Count == 0)
+					return false;
+
+				this.currentSelection++;
+				if(this.currentSelection >= this.optionsMap.Count)
+					this.currentSelection = 0;
+				return true;
+			default:
+				return false;
+		}
+	}
+}
+
+public enum MenuCode : byte{
+	MAIN,
+	SINGLEPLAYER,
+	MULTIPLAYER,
+	OPTIONS
 }
