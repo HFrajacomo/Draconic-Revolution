097777d1a895701d9dc96d9e751d251bf7add315
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2023-03-24 04:06:27 -0300

    Dropped Item mesh overhaul
    - New minimalistic geometry given to Dropped Items 
    - New UV remapping for Icon Atlas
    - New Icon Shader and Material

--- Assets/Scripts/ItemEntity.cs
+++ Assets/Scripts/ItemEntity.cs
@@ -9,27 +9,30 @@ using Random = System.Random;
 
 public class ItemEntity : MonoBehaviour
 {
-	private static string NAME = "DroppedItem";
-	private static Random rng = new Random();
+	private static readonly string NAME = "DroppedItem";
+	private static readonly Random rng = new Random();
 
 	public TimeOfDay time;
 	public MeshFilter meshFilter;
 	private Mesh mesh;
 	private ItemStack its;
 	public GameObject go;
-	private Rigidbody rb;
     public GameObject droppedItemHierarchy;
 	private Vector3 initialForce = new Vector3(0f, 0f, 0f);
-	private string timeToRelease;
 
 	void Start(){
 		this.go = this.gameObject;
-		this.go.transform.parent = this.droppedItemHierarchy.transform;
+		//this.go.transform.parent = this.droppedItemHierarchy.transform;
 		this.meshFilter = this.go.GetComponent<MeshFilter>();
 		this.go.name = ItemEntity.NAME;
 		this.mesh = new Mesh();
 
 		BuildMesh();
+	}
+
+	void OnDestroy(){
+		this.mesh.Clear();
+		this.mesh = null;
 	}
 
 	public void RandomForce(){
@@ -44,17 +47,8 @@ public class ItemEntity : MonoBehaviour
 		this.initialForce = force;
 	}
 
-	private void ApplyForce(){
-		this.rb.useGravity = true;
-		this.rb.AddForce(this.initialForce);
-	}
-
 	public void SetItemStack(ItemStack its){
 		this.its = its;
-	}
-
-	public void SetTime(){
-		this.timeToRelease = time.FakeSum(30);
 	}
 
 
@@ -71,6 +65,7 @@ public class ItemEntity : MonoBehaviour
 		this.mesh.SetTriangles(ItemMeshData.materialTris, 1);
 
 		UpdateMeshUV(this.its.GetIconID());
+		this.mesh.RecalculateNormals();
 
 		this.meshFilter.sharedMesh = this.mesh;
 	}
@@ -86,49 +81,6 @@ public class ItemEntity : MonoBehaviour
 	public ItemStack GetItemStack(){
 		return this.its;
 	}
-
-	/*
-	private void OnTriggerEnter(Collider other){
-		if(this.its == null)
-			return;
-
-		byte itemsTaken = 0;
-
-		if(other.tag == "Player"){
-			if(this.timeToRelease != null){
-				if(!time.IsPast(this.timeToRelease)){
-					return;
-				}
-			}
-
-			PlayerEvents pe = other.GetComponent<PlayerEvents>();
-
-			itemsTaken = pe.hotbar.AddStack(this.its, pe.hotbar.CanFit(this.its));
-			if(itemsTaken >= 0)
-				pe.DrawHotbar();
-
-			if(CheckDestroy(itemsTaken))
-				return;
-
-			itemsTaken += pe.inventory.AddStack(this.its, pe.inventory.CanFit(this.its));
-
-			if(CheckDestroy(itemsTaken))
-				return;
-		}
-		else if(other.tag == "DroppedItem"){
-			if(this.rb.IsSleeping() == false)
-				return;
-
-			ItemEntity ie = other.GetComponent<ItemEntity>();
-			ItemStack itemStack = ie.GetItemStack();
-
-			if(this.its.MoveTo(ref itemStack)){
-				this.its = null;
-				GameObject.Destroy(this.go);
-			}
-		}
-	}
-	*/
 
 	private bool CheckDestroy(byte itemsTaken){
 		if(itemsTaken >= this.its.GetAmount()){
--- Assets/Scripts/TESTONLY.cs
+++ Assets/Scripts/TESTONLY.cs
@@ -6,8 +6,13 @@ using Unity.Collections;
 
 public class TESTONLY : MonoBehaviour
 {   
+	public GameObject itemPrefab;
+	private ItemEntity ie;
+	private ItemStack its;
+
 	public void Start(){
-		EnvironmentVariablesCentral.PrintDirectories();
-		Application.Quit();
+		this.its = new ItemStack(ItemID.GRASSBLOCK, 10);
+		this.ie = GameObject.Instantiate(this.itemPrefab, new Vector3(0, 3, 0), Quaternion.Euler(0, 0, 0)).GetComponent<ItemEntity>();
+		this.ie.SetItemStack(its);
 	}
 }
