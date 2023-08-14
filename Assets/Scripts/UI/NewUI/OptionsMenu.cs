8ced07ac118e3ad391431233070c737561be731b
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2023-08-13 21:37:24 -0300

    Added ArrowHighlight shader

---
+++ Assets/Scripts/UI/NewUI/OptionsMenu.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : Menu
+{
+	// Tabs
+	public GameObject gameTab;
+	public GameObject videoTab;
+	public GameObject audioTab;
+	public GameObject adminTab;
+}
--- Assets/Scripts/UI/SliderVisorUtil.cs
+++ Assets/Scripts/UI/SliderVisorUtil.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 /*
 This utility can be added to any Slider to make it update a readonly InputField with it's value
@@ -11,14 +10,14 @@ The InputField must be a child of the Slider GameObject
 public class SliderVisorUtil : MonoBehaviour
 {
     public Slider slider;
-    public TMP_InputField inputField;
+    public InputField inputField;
 
     /*
     Sets the Slider and InputField on Runtime
     */
     public void Awake(){
         this.slider = this.gameObject.GetComponent<Slider>();
-        this.inputField = this.gameObject.GetComponentInChildren<TMP_InputField>();
+        this.inputField = this.gameObject.GetComponentInChildren<InputField>();
         this.inputField.readOnly = true;
         this.inputField.text = ((int)this.slider.value).ToString();
     }
